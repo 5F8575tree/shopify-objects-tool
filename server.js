f20c6a9e76e476a9d1f781be06000e6a293c52a9
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2024-01-26 14:11:38 +0900

    add endpoints for storefront api

--- server.js
+++ server.js
@@ -69,6 +69,29 @@ app.get('/shopify/collections/:collectionId/products', async (req, res) => {
   }
 });
 
+// Configure Axios for Shopify Storefront API
+const storefrontAxios = axios.create({
+  baseURL: process.env.SHOPIFY_STORE_URL,
+  headers: {
+    'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_STOREFRONT_API_KEY,
+    'Content-Type': 'application/graphql'
+  }
+});
+
+// Endpoint for Storefront API GraphQL queries
+app.get('/storefront/:query', async (req, res) => {
+  try {
+    // Construct GraphQL query from request parameter
+    const graphqlQuery = { query: decodeURIComponent(req.params.query) };
+
+    const response = await storefrontAxios.post('', graphqlQuery);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error with Storefront API request:', error.response || error);
+    res.status(500).send('Error with Storefront API request');
+  }
+});
+
 
 
 app.listen(port, () => {
